c3163e4b7778dff80e55b7524cd1f821ff5b4112
Andrea Rompani <rompamail2@gmail.com>
2025-10-25 13:47:23 +0200

    refactor: update device name to improve clarity

--- custom_components/manual_heat_cost_allocator/device.py
+++ custom_components/manual_heat_cost_allocator/device.py
@@ -3,7 +3,7 @@ from .const import DOMAIN
 def get_device_info(prefix, config_entry_id, area_id=None):
     return {
         "identifiers": {(DOMAIN, config_entry_id)},
-        "name": f"{prefix} Heat Cost Allocator",
+        "name": f"{prefix} Meter",
         "manufacturer": "Manual Heat Cost Allocator",
         "model": "Manual Heat Cost Allocator",
     }
